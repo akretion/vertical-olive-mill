010bace190cf85e4203073b7935f69c02428461a
Alexis de Lattre <alexis.delattre@akretion.com>
2018-10-22 16:20:42 +0200

    Update organic certifying entities

--- olive_mill/models/organic_certifying_entity.py
+++ olive_mill/models/organic_certifying_entity.py
@@ -3,7 +3,7 @@
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import api, fields, models
 
 
 class OrganicCertifyingEntity(models.Model):
@@ -12,10 +12,18 @@ class OrganicCertifyingEntity(models.Model):
     _order = 'sequence'
 
     name = fields.Char(string='Name', required=True)
+    code = fields.Char(string='Code', required=True)
     sequence = fields.Integer()
     active = fields.Boolean(default=True)
 
     _sql_constraints = [(
-        'name_unique',
-        'unique(name)',
-        'This organic certifying entity already exists.')]
+        'code_unique',
+        'unique(code)',
+        'This organic certifying entity code already exists.')]
+
+    @api.depends('name', 'code')
+    def name_get(self):
+        res = []
+        for entity in self:
+            res.append((entity.id, '%s (%s)' % (entity.code, entity.name)))
+        return res
