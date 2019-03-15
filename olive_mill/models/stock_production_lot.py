7917d8928401ca7233177cd94e497d4f9a243d36
Alexis de Lattre <alexis.delattre@akretion.com>
2019-03-15 14:13:10 +0100

    Add tracability report

--- olive_mill/models/stock_production_lot.py
+++ olive_mill/models/stock_production_lot.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 Barroux Abbey (https://www.barroux.org/)
+# Copyright 2018-2019 Barroux Abbey (https://www.barroux.org/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
 
 
 class StockProductionLot(models.Model):
@@ -23,4 +24,42 @@ class StockProductionLot(models.Model):
             if lot.olive_production_id:
                 dname = u'%s (%s)' % (dname, lot.olive_production_id.farmers)
             res.append((lot.id, dname))
+        return res
+
+    @api.model
+    def browse_recursive_tree(self, root_quant, lines):
+        if root_quant.product_id and root_quant.product_id.olive_type == 'oil':
+            lot = root_quant.lot_id
+            if not lot:
+                    raise UserError(_(
+                        "The quant ID %d of the olive oil product '%s' "
+                        "is not linked to a lot !") % (
+                            root_quant.id, root_quant.product_id.display_name))
+            if lot.olive_production_id:
+                for line in lot.olive_production_id.line_ids:
+                    lines[line] = True
+            elif root_quant.consumed_quant_ids:
+                for cquant in root_quant.consumed_quant_ids:
+                    self.browse_recursive_tree(cquant, lines)
+
+    def report_get_arrival_lines(self):
+        self.ensure_one()
+        if not self.product_id.olive_type:
+            raise UserError(_(
+                "The product '%s' has no olive type.")
+                % self.product_id.display_name)
+        if self.product_id.olive_type not in ('bottle_full', 'oil'):
+            raise UserError(_(
+                "The product '%s' has an olive type '%s'. This report is only "
+                "for products with olive type 'Full Oil Bottle' or "
+                "Olive Oil'.") % (
+                    self.product_id.display_name, self.product_id.olive_type))
+        if not self.quant_ids:
+            raise UserError(_(
+                "The production lot '%s' is not linked to a quant.")
+                % self.display_name)
+        quant = self.quant_ids[0]
+        lines = {}
+        self.browse_recursive_tree(quant, lines)
+        res = sorted(lines.keys(), key=lambda to_sort: to_sort.arrival_date)
         return res
