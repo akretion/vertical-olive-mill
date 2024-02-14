53da86bc9c386006b78942b32ee5037190b1e0b5
Alexis de Lattre <alexis.delattre@akretion.com>
2024-02-14 19:15:17 +0100

    FIX migration script

--- olive_mill/migrations/14.0.1.0.0/pre-migration.py
+++ olive_mill/migrations/14.0.1.0.0/pre-migration.py
@@ -13,7 +13,6 @@ _column_renames = {
 
 @openupgrade.migrate()
 def migrate(env, version):
-    openupgrade.rename_columns(env.cr, _column_renames)
     openupgrade.logged_query(env.cr, "UPDATE product_template set detailed_type='olive_oil' WHERE olive_type='oil'")
     openupgrade.logged_query(env.cr, "UPDATE product_template set detailed_type='olive_tax' WHERE olive_type='tax'")
     openupgrade.logged_query(env.cr, "UPDATE product_template set detailed_type='olive_service' WHERE olive_type='service'")
@@ -24,3 +23,4 @@ def migrate(env, version):
     openupgrade.logged_query(env.cr, "UPDATE product_template set detailed_type='olive_bottle_full' WHERE olive_type='bottle_full'")
     openupgrade.logged_query(env.cr, "UPDATE product_template set detailed_type='olive_bottle_empty' WHERE olive_type='bottle' AND type='product'")
     openupgrade.logged_query(env.cr, "UPDATE product_template set detailed_type='olive_barrel_farmer', type='product' WHERE olive_type='bottle' AND type='consu'")
+    openupgrade.rename_columns(env.cr, _column_renames)
